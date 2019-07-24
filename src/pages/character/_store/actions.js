1834f540ac84bfb223e1ce8d922a93290b8fdb3a
newaeonweb <fernando@newaeonweb.com.br>
2019-07-24 18:10:10 -0300

    using service

--- src/pages/character/_store/actions.js
+++ src/pages/character/_store/actions.js
@@ -1,16 +1,23 @@
 import { CHARACTER_GET, CHARACTER_SUCCESS, CHARACTER_FAIL } from './types';
+import { httpCartoonService } from '../../_services/http-cartoon.service';
 
 export const getCharacters = () => dispatch => {
   dispatch({
     type: CHARACTER_GET,
     isLoading: true,
   });
-  fetch('https://rickandmortyapi.com/api/character')
-    .then(res => res.json())
-    .then(res =>
+  httpCartoonService.getList('character').then(
+    res => {
       dispatch({
         type: CHARACTER_SUCCESS,
         payload: res,
-      })
-    );
+      });
+    },
+    error => {
+      dispatch({
+        type: CHARACTER_FAIL,
+        errorMessage: error,
+      });
+    }
+  );
 };
