41e9bbfcdad58297af59f598563ec3e36bb01b90
newaeonweb <fernando@newaeonweb.com.br>
2019-07-29 14:11:35 -0300

    added logger

--- src/app-store/app-store.js
+++ src/app-store/app-store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import appReducer from './app-reducer';
 
 const initialState = {};
-const middleware = [thunk];
+const middleware = [thunk, logger];
 
 const store = createStore(
   appReducer,
